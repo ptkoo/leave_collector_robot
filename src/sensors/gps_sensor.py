03f9fb5b203b681d7250f3138ae7c4115207b045
Tanakorn Youngmeesuk <phon.youngmeesuk@gmail.com>
2023-10-12 16:30:13 +0700

    try fix

--- src/sensors/gps_sensor.py
+++ src/sensors/gps_sensor.py
@@ -1,58 +1,52 @@
 import serial
 import pynmea2
 import time
+import io
 
-from gyroscope_sensor import GyroscopeSensor
-
+# from gyroscope_sensor import GyroscopeSensor
 class GPSSensor:
     def __init__(self, port, baudrate):
         self.port = port
         self.baudrate = baudrate
-        self.ser = serial.Serial(port, baudrate, timeout=5)
-
+        self.ser = serial.Serial(port, baudrate, timeout=5.0)
+        self.sio = io.TextIOWrapper(io.BufferedRWPair(self.ser, self.ser))
 
     def read(self):
-        data = self.ser.readline().decode("utf-8")
-        if data[0:6] == "$GPGGA":
-            msg = pynmea2.parse(data)
+        try:
+            line = self.sio.readline()
+            msg = pynmea2.parse(line)
             return self.parse(msg)
-        else:
+        except serial.SerialException as e:
+            print('Device error: {}'.format(e))
             return None
-        
+        except pynmea2.ParseError as e:
+            print('Parse error: {}'.format(e))
+            return None
+
     def parse(self, msg):
         lat = msg.latitude
         lon = msg.longitude
         alt = msg.altitude
         speed = msg.spd_over_grnd
-        return data(lat, lon, alt, speed)
-    
+        num_sats = msg.num_sats
+        hdop = msg.horizontal_dil
+        return (lat, lon, alt, speed, num_sats, hdop)
+
     def close(self):
         self.ser.close()
 
 
-
-class data:
-    def __init__(self, lat, lon, alt, speed):
-        self.lat = lat
-        self.lon = lon
-        self.alt = alt
-        self.speed = speed
-
-    def __str__(self):
-        return "lat: " + str(self.lat) + " lon: " + str(self.lon) + " alt: " + str(self.alt) + " speed: " + str(self.speed)
+if __name__ == '__main__':
+    gps = GPSSensor('/dev/ttyS0', 9600)
+    try:
+        while True:
+            data = gps.read()
+            if data is not None:
+                lat, lon, alt, speed, num_sats, hdop = data
+                print('Latitude: {}, Longitude: {}, Altitude: {}, Speed: {}, Num Sats: {}, HDOP: {}'.format(lat, lon, alt, speed, num_sats, hdop))
+            time.sleep(1)
     
-    
-
-
-
-if __name__ == "__main__":
-
-    sensor = GPSSensor("/dev/ttyAMAO", 9600)
-    while True:
-        data = sensor.read()
-        print(data)
-        time.sleep(1)
-        #if data corrupt break
-        if data == None:
-            break
-    sensor.close()
+    except KeyboardInterrupt:
+        gps.close()
+        print('Bye.')
+        
