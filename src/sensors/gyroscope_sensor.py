410ff97abc69a2e6ce341ba96a4a81d98ad968d9
ptkoo <paingthatkoo@gmail.com>
2023-10-21 06:27:19 +0700

    Calibration Code for magnetometer

--- src/sensors/gyroscope_sensor.py
+++ src/sensors/gyroscope_sensor.py
@@ -21,7 +21,7 @@ def MPU_Init():
     bus.write_byte_data(Device_Address, SMPLRT_DIV, 7)
     bus.write_byte_data(Device_Address, PWR_MGMT_1, 1)
     bus.write_byte_data(Device_Address, CONFIG, 0)
-    bus.write_byte_data(Device_Address, GYRO_CONFIG, 24)
+    bus.write_byte_data(Device_Address, GYRO_CONFIG, 8)
     bus.write_byte_data(Device_Address, INT_ENABLE, 1)
  
 def read_raw_data(addr):
@@ -50,21 +50,26 @@ if __name__ == "__main__":
     MPU_Init()
      
     print("Reading MPU6050...")
+    # Constants
+    gyro_scale = 65.5  # Sensitivity scale factor for ±250 dps range
+    dt = 0.5  # Time interval in seconds (sampling rate of 2 Hz)
+
+    # Variables to store previous gyro values
+    gyro_x_prev = 0.0
+    gyro_y_prev = 0.0
+    gyro_z_prev = 0.0
+
+    # Variables to store angles
+    x_angle = 0.0
+    y_angle = 0.0
+    z_angle = 0.0
     try:
         while True:
+                
+        
             acc_x = read_raw_data(ACCEL_XOUT_H)
             acc_y = read_raw_data(ACCEL_YOUT_H)
             acc_z = read_raw_data(ACCEL_ZOUT_H)
-            
-            gyro_x = read_raw_data(GYRO_XOUT_H)
-            gyro_y = read_raw_data(GYRO_YOUT_H)
-            gyro_z = read_raw_data(GYRO_ZOUT_H)
-            
-            gyro_scale = 131.0  # Sensitivity scale factor for ±250 dps range
-
-            gyro_x_scaled = gyro_x / gyro_scale  # Angular velocity in degrees per second
-            gyro_y_scaled = gyro_y / gyro_scale
-            gyro_z_scaled = gyro_z / gyro_scale
 
             acclX_scaled = acc_x * 1/16384 * 9.80665
             acclY_scaled = acc_y * 1/16384 * 9.80665
@@ -74,12 +79,29 @@ if __name__ == "__main__":
             y_angle = get_y_rotation(acclX_scaled, acclY_scaled, acclZ_scaled)
             
             
-            print("X gyro: ",gyro_x_scaled)
-            print("Y gyro: ",gyro_y_scaled)
-            print("Z gyro: ",gyro_z_scaled)
+            # Read gyroscope raw data
+            gyro_x = read_raw_data(GYRO_XOUT_H)
+            gyro_y = read_raw_data(GYRO_YOUT_H)
+            gyro_z = read_raw_data(GYRO_ZOUT_H)
+        
+            # Scale gyro data to degrees per second
+            gyro_x_scaled = gyro_x / gyro_scale  # Angular velocity in degrees per second
+            gyro_y_scaled = gyro_y / gyro_scale
+            gyro_z_scaled = gyro_z / gyro_scale
+        
+            # Integrate gyro data to obtain angles
+            x_angle += gyro_x_scaled * dt
+            y_angle += gyro_y_scaled * dt
+            z_angle += gyro_z_scaled * dt
+        
+            print("X Angle: {:.2f} degrees".format(x_angle))
+            print("Y Angle: {:.2f} degrees".format(y_angle))
+            print("Z Angle: {:.2f} degrees".format(z_angle))
             print("\n")
-            
-            sleep(.50)
+        
+           
+        
+            sleep(dt)
     except KeyboardInterrupt:
         sys.exit(0)
     except Exception as e:
--- src/sensors/magnetometer.py
+++ src/sensors/magnetometer.py
@@ -5,6 +5,11 @@
 import smbus		#import SMBus module of I2C
 from time import sleep  #import sleep
 import math
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import matplotlib.transforms as transforms
+from matplotlib.patches import Ellipse
+import numpy as np
 
 #some MPU6050 Registers and their Address
 Register_A     = 0              #Address of Configuration register A
@@ -17,6 +22,24 @@ Y_axis_H    = 0x07              #Address of Y-axis MSB data register
 declination = -0.0151261874        #define declination angle of location where measurement going to be done
 pi          = 3.14159265359     #define pi value
 
+# Initialize a list to store raw data samples
+x_raw_samples = []
+y_raw_samples = []
+z_raw_samples = []
+
+# Calibrate sample
+
+x_calibrated_samples = []
+y_calibrated_samples = []
+z_calibrated_samples = []
+
+# This is the calibration data for transformation matrix 
+x_calibrated_samples1 = []
+y_calibrated_samples1 = []
+# bias
+x_raw_bias = 0
+y_raw_bias = 0
+z_raw_bias = 0
 
 def Magnetometer_Init():
         #write to Configuration Register A
@@ -44,23 +67,225 @@ def read_raw_data(addr):
             value = value - 65536
         return value
 
+def confidence_ellipse(x, y, ax, n_std=3.0, facecolor='none', **kwargs):
+   
+
+    cov = np.cov(x, y)
+    pearson = cov[0, 1]/np.sqrt(cov[0, 0] * cov[1, 1])
+    # Using a special case to obtain the eigenvalues of this
+    # two-dimensional dataset.
+    ell_radius_x = np.sqrt(1 + pearson) #small radius (major)
+    ell_radius_y = np.sqrt(1 - pearson) #large radius (minor)
+    
+    ellipse = Ellipse((0, 0), width = ell_radius_x *2, height = ell_radius_y *2,
+                      facecolor=facecolor, **kwargs)
+
+    # Calculating the standard deviation of x from
+    # the squareroot of the variance and multiplying
+    # with the given number of standard deviations.
+    scale_x = np.sqrt(cov[0, 0]) * n_std
+    mean_x = np.mean(x)
+
+    # calculating the standard deviation of y ...
+    scale_y = np.sqrt(cov[1, 1]) * n_std
+    mean_y = np.mean(y)
+    
+    transf = transforms.Affine2D() \
+       .rotate_deg(0) \
+       .scale(scale_x, scale_y) \
+       .translate(mean_x, mean_y)
+
+    ellipse.set_transform(transf + ax.transData)
+   
+    # Calculate width and height of transformed ellipse
+
+    width_transformed = ell_radius_x*2* scale_x
+    height_transformed = ell_radius_y*2* scale_y
+
+    # Get the transformation matrix
+    TandT_matrix = transf.get_matrix()
+    
+    #print(TandT_matrix)
+    transformationMatrix = TandT_matrix[:2, :2]  # Extract 2x2 transformation matrix
+    
+    # Extract translation matrix
+    translationMatrix = TandT_matrix[:2, 2:]
+    #print("Transformation Matrix\n", transformationMatrix)
+    #print("Translation Matrix\n", translationMatrix)
+    
+    # Calculate the major and minor axes endpoints in data coordinates
+    major_axis_endpoints_data = np.array([[ell_radius_x / 2, 0], [-ell_radius_x / 2, 0]])
+    minor_axis_endpoints_data = np.array([[0, ell_radius_y / 2], [0, -ell_radius_y / 2]])
+
+    
+    # Apply the transformation matrix to the endpoints
+    major_axis_endpoints_transformed = np.dot(major_axis_endpoints_data, transformationMatrix) + translationMatrix.T
+    minor_axis_endpoints_transformed = np.dot(minor_axis_endpoints_data, transformationMatrix) + translationMatrix.T
+        
+    
+    #print("Major Axis\n", major_axis_endpoints_transformed)
+    #print("Minor Axis\n", minor_axis_endpoints_transformed)
+    
+    # Extract the transformed endpoints
+    major_axis_endpoint1, major_axis_endpoint2 = major_axis_endpoints_transformed
+    minor_axis_endpoint1, minor_axis_endpoint2 = minor_axis_endpoints_transformed
+
+
+    
+    return ax.add_patch(ellipse), width_transformed, height_transformed , major_axis_endpoint1, major_axis_endpoint2, minor_axis_endpoint1, minor_axis_endpoint2
+    
+def calibrate():
+        print( " Calcuting bias... " )
+        # Read 1000 samples of raw data
+        for _ in range(500):
+            x_raw = read_raw_data(X_axis_H)
+            y_raw = read_raw_data(Y_axis_H)
+            z_raw = read_raw_data(Z_axis_H)
+
+            # Append raw data to the sample lists
+            x_raw_samples.append(x_raw)
+            y_raw_samples.append(y_raw)
+            z_raw_samples.append(z_raw)
+
+            # Calculate mean of raw data samples
+            x_raw_bias = sum(x_raw_samples) / len(x_raw_samples)
+            y_raw_bias = sum(y_raw_samples) / len(y_raw_samples)
+            z_raw_bias = sum(z_raw_samples) / len(z_raw_samples)
+
+                
+            # Append Calibrated data to the calibrated lists
+            x_calibrated_samples.append(x_raw - x_raw_bias )
+            y_calibrated_samples.append(y_raw - y_raw_bias )
+            z_calibrated_samples.append(z_raw - z_raw_bias )
+            
+            sleep(0.1)
+            
+        print("Calibrated")   
+        
+        # Plot the confidence ellipse with 3 standard deviations
+        ax = plt.gca()
+        ellipse, width, height, major_axis_endpoint1, major_axis_endpoint2, minor_axis_endpoint1, minor_axis_endpoint2= confidence_ellipse(x_calibrated_samples, y_calibrated_samples, ax, n_std=2.5, edgecolor='red', linewidth=2)
+     
+        # Get the center point and orientation angle of the ellipse
+        ellipse_center = ellipse.center
+        # orientation_rad = np.arctan2(height, width)
+
+        
+        
+        
+       # Plot rotated major axis (in blue) and minor axis (in green)
+        ax.plot([major_axis_endpoint1[0], major_axis_endpoint2[0]],
+                [major_axis_endpoint1[1], major_axis_endpoint2[1]], color='blue', linestyle='--', label='Major Axis')
+
+        ax.plot([minor_axis_endpoint1[0], minor_axis_endpoint2[0]],
+                [minor_axis_endpoint1[1], minor_axis_endpoint2[1]], color='green', linestyle='--', label='Minor Axis')
+
+
+        # Plot the center point
+        plt.plot(ellipse_center[0], ellipse_center[1], marker='o', markersize=8, color='yellow', label='Ellipse Center')
+
+        ax.scatter(x_raw_samples, y_raw_samples, c='b', marker='o')
+        ax.scatter(x_calibrated_samples, y_calibrated_samples , c='r', marker='o', label='Calibrated Data')
+        
+        # So, we are morphing into circle from the ellipse.
+       
+        # Scale factor
+        #scale_factor = width / height
+        
+        scale_factor = math.sqrt((( major_axis_endpoint1[1]- major_axis_endpoint1[0])**2) +  ((major_axis_endpoint2[1] - major_axis_endpoint2[0]) ** 2)) \
+        / math.sqrt (((minor_axis_endpoint1[1]- minor_axis_endpoint1[0]) ** 2 ) +  (( minor_axis_endpoint2[1]- minor_axis_endpoint2[0]) ** 2 ))
+        
+        
+        scaleMatrix = np.array([[1/scale_factor, 0], [0, 1]])
+       
+       # for x in x_calibrated_samples:
+                #x_calibrated_samples1.append(x / scale_factor)
+        
+        # Transfoming ellipse into circle
+        
+        # Create Rotational Matrix
+        c, s = np.cos(0), np.sin(0)
+        rotationalMatrix = np.array([[c, s], [-s, c]])
+
+        # Create Reverse Rotational Matrix
+        inverse_rotation_matrix = rotationalMatrix.T
+        
+        soft_factor = rotationalMatrix @ scaleMatrix @ inverse_rotation_matrix
+        
+        for x_calibrated, y_calibrated in zip(x_calibrated_samples, y_calibrated_samples):
+                
+                # Create a 2 x 1 matrix with x and y as elements
+                vectorMatrix = np.array([[x_calibrated,0], [0,y_calibrated]])
+                
+                # Perform matrix multiplication: Reverse Rotation Matrix * Scale Matrix * Rotation Matrix * Vector Matrix
+               
+                resultMatrix = vectorMatrix @ soft_factor
+                
+                # Extract individual elements from the result matrix
+                x_calibrated_samples1.append(resultMatrix[0, 0] ) # First element (x value)
+                y_calibrated_samples1.append(resultMatrix[1, 1] ) # Second element (y value)
+                
+        
+        ellipse1 = confidence_ellipse(x_calibrated_samples1, y_calibrated_samples1, ax, n_std=2.5, edgecolor='green', linewidth=2)
+        ax.scatter(x_calibrated_samples1, y_calibrated_samples1 , c='c', marker='o', label='Calibrated Data')
+        plt.xlim(-1000, 1000)  
+        plt.ylim(-1000, 1000)  
+
+        ax.set_xlabel('X Raw')
+        ax.set_ylabel('Y Raw')
+        # Show legend
+        plt.legend()
+        plt.show()
+        
+
+        # We return this so that we can calibrate the raw data. These results came from the transformation and bias calculation
+        return x_raw_bias, y_raw_bias, soft_factor
+        
+        
 
 bus = smbus.SMBus(1) 	# or bus = smbus.SMBus(0) for older version boards
 Device_Address = 0x1e   # HMC5883L magnetometer device address
 
 Magnetometer_Init()     # initialize HMC5883L magnetometer 
 
-print (" Reading Heading Angle")
+# Assigning to global variables 
+
+x_raw_bias, y_raw_bias, soft_factor = calibrate()
+
+
 
 while True:
     
 	
-        #Read Accelerometer raw value
-        x = math.radians(read_raw_data(X_axis_H))
-        z = math.radians(read_raw_data(Z_axis_H))
-        y = math.radians(read_raw_data(Y_axis_H))
+        #Read magnetometer raw value
+        
+        x_raw = read_raw_data(X_axis_H) 
+        y_raw = read_raw_data(Y_axis_H) 
+        z_raw = read_raw_data(Z_axis_H) 
+        
+        
+        # Read Calibrated Accelerometer value
 
-        heading = math.atan2(y,x) + declination
+        x_calibrated = x_raw - x_raw_bias
+        y_calibrated = y_raw - y_raw_bias
+        z_calibrated = z_raw - z_raw_bias
+        
+       
+        # Create a 2 x 1 matrix with x and y as elements
+        vectorMatrix = np.array([[x_calibrated,0], [0,y_calibrated]])
+
+       
+
+        # Perform matrix multiplication: Reverse Rotation Matrix * Scale Matrix * Rotation Matrix * Vector Matrix
+        resultMatrix = vectorMatrix @ soft_factor
+
+        # Extract individual elements from the result matrix
+        x = resultMatrix[0, 0]  # First element (x value)
+        y = resultMatrix[1, 1]  # Second element (y value)
+
+        
+
+        heading = - math.atan2(x,y) 
         
         #Due to declination check for >360 degree
         if(heading > 2*pi):
@@ -71,7 +296,8 @@ while True:
                 heading = heading + 2*pi
 
         #convert into angle
-        heading_angle = int(heading * 180/pi)
-
+        heading_angle = int(heading * 180/pi) 
+        
         print ("Heading Angle = %d°" %heading_angle)
-        sleep(1)
+        sleep(0.2)
+   
